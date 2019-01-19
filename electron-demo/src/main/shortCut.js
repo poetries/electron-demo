4ca75cc77221bd6240b8539085163086ec7452e9
poetries <jingguanliuye@gmail.com>
2019-01-19 10:04:50 +0800

    update

--- electron-demo/src/main/shortCut.js
+++ electron-demo/src/main/shortCut.js
@@ -2,3 +2,13 @@
 
 var {globalShortcut, app} = require('electron')
 
+app.on('ready', ()=>{
+    // 注册全局快捷键
+    globalShortcut.register('command+4', ()=>{
+        console.log(1)
+    })
+
+    // 检测快捷键是否注册成功 true是注册成功
+    let isRegister = globalShortcut.isRegistered('command+4')
+    console.log(isRegister)
+})
