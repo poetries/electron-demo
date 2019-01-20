4bc1b6774d949281a31d12ec15ec836c48e9e465
poetries <jingguanliuye@gmail.com>
2019-01-20 18:31:32 +0800

    update

---
+++ electron-vue-demo/src/renderer/components/News.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="wrapper">
+    news page
+    <div>
+         <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'news-page',
+    components: {  }
+  }
+</script>
